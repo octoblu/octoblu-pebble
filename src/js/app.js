f2380d01c0efd7cca2434d8a40e81af6767cb12c
Peter DeMartini <peterdemartini@me.com>
2014-07-28 18:04:30 -0700

    Working pebble app, uses AJAX instead of sockets

--- src/js/app.js
+++ src/js/app.js
@@ -3,21 +3,37 @@
  */
 
 var UI = require('ui'),
-    connect = require('./connect.js'),
+    skynet = require('./skynet.js'),
     title = 'Octoblu';
 
 var main = new UI.Card({
-  title: title,
-  icon: 'images/menu_icon.png',
-  body: 'Connecting to Meshblu...'
+    title: title,
+    icon: 'images/menu_icon.png',
+    body: 'Connecting to Meshblu...'
 });
 
 main.show();
 
-var conn = connect(function(err, data){
-    if(err){
-        main.body(err);
-    }else{
-        main.body('Connected Meshblu');
-    }
+skynet.status(function (data) {
+    main.subtitle('Skynet: ' + data.skynet);
 });
+
+function showCard(title, msg){
+    var card = new UI.Card();
+    card.title(title);
+    card.body(msg);
+    card.show();
+};
+
+skynet.connect(function (data) {
+
+    var msg = 'Connected to Skynet';
+    main.body(msg);
+
+    main.on('click', 'select', function (e) {
+        console.log('Clicked Select');
+        showCard('Skynet UUID', data.uuid);
+    });
+
+});
+
